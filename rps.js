f1c1fc827600b978619aacb1c33ed1eb1d086345
Matthew Cervantes <96374397+mtcerv23@users.noreply.github.com>
2023-03-01 17:05:29 -0800

    Added UI plus reset button

--- rps.js
+++ rps.js
@@ -1,3 +1,22 @@
+const buttons = document.querySelectorAll('.game');
+const div = document.querySelector('div');
+const reset = document.querySelector('.reset');
+
+div.style.paddingTop = '50px';
+
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {game(button.textContent.toLowerCase());})
+})
+
+reset.addEventListener('click', () => {
+  playerScore = 0;
+  computerScore = 0;
+  div.innerHTML = ''});
+
+
 function getComputerChoice() {
   let choices = ['rock', 'paper', 'scissors'];
   const computerChoice = choices[Math.floor(Math.random() * choices.length)];
@@ -7,58 +26,44 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   let win = "lose";
   let winner;
-  if (computerSelection == 'rock' && playerSelection == 'paper') {
+  if (playerSelection == 'paper' && computerSelection == 'rock') {
     win = "win";
-  } else if (computerSelection == 'paper' && playerSelection == 'scissors') {
+  } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
     win = "win";
-  } else if (computerSelection == 'scissors' && playerSelection == 'rock') {
+  } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
     win = "win";
   } else if (computerSelection === playerSelection) {
-      win = "tie";
+    win = "tie";
     }
     
-
   if (win === "lose") {
-    console.log(`You ${win}! ${computerSelection} beats ${playerSelection}`);
+    div.innerHTML += `You ${win}! ${computerSelection} beats ${playerSelection}<br>`;
     winner = "computer";
     return winner;
   } else if (win === "tie") {
-    console.log(`Tie! You both picked ${playerSelection}`);
+    div.innerHTML += `Tie! You both picked ${playerSelection}<br>`;
     return;
   } else {
-    console.log(`You ${win}! ${playerSelection} beats ${computerSelection}`);
+    div.innerHTML += `You ${win}! ${playerSelection} beats ${computerSelection}<br>`;
     winner = "player";
     return winner;
   }
 }
 
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  let playerSelection;
-  let computerSelection;
-  let winner;
+function game(playerSelection) {
+  if (playerScore == 5 || computerScore == 5) return;
 
-  for (let i = 0; i < 5; i++) {
-    playerSelection = prompt("Rock, paper or scissors? ");
-    console.log(playerSelection);
-    computerSelection = getComputerChoice();
-    winner = playRound(playerSelection, computerSelection);
-    if (winner === "player") {
-      playerScore++;
-    } else if (winner === "computer") {
-      computerScore++;
-    }
-    console.log(`You: ${playerScore}, Computer: ${computerScore}`);
+  else {
+    div.innerHTML += `You chose: ${playerSelection}<br>`;
+    let computerSelection = getComputerChoice();
+    let winner = playRound(playerSelection, computerSelection);
+
+    if (winner === "player") playerScore++;
+    else if (winner === "computer") computerScore++;
+
+    div.innerHTML += `You: ${playerScore}, Computer: ${computerScore}<br><br>`;
+
+    if (playerScore === 5) div.innerHTML += "Congratulations! You win!<br><br>";
+    else if (computerScore === 5) div.innerHTML += "Better luck next time!<br><br>";
   }
-
-  if (playerScore > computerScore) {
-    console.log("Congratulations! You win!");
-  } else if (playerScore === computerScore) {
-    console.log("It's a tie!");
-  }
-  else console.log("Better luck next time!");
-
 }
-
-game();
